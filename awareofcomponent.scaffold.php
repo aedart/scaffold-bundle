605175521ccf4b6112cb4cad1a17dc0f4870e360
Alin Eugen Deac <aedart@gmail.com>
2016-10-01 10:34:00 +0200

    Fix generating unwanted directories in project
    This was some leftover code from when scaffold was in the aedart/model
    package.

--- awareofcomponent.scaffold.php
+++ awareofcomponent.scaffold.php
@@ -77,20 +77,20 @@ return [
     | ...etc
     */
     'folders' => [
-        'src' => [
-            'Contracts' => [
-                'Arrays',
-                'Booleans',
-                'Integers',
-                'Strings',
-            ],
-            'Traits' => [
-                'Arrays',
-                'Booleans',
-                'Integers',
-                'Strings',
-            ]
-        ]
+//        'src' => [
+//            'Contracts' => [
+//                'Arrays',
+//                'Booleans',
+//                'Integers',
+//                'Strings',
+//            ],
+//            'Traits' => [
+//                'Arrays',
+//                'Booleans',
+//                'Integers',
+//                'Strings',
+//            ]
+//        ]
     ],
 
     /* ------------------------------------------------------------
